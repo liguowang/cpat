9df2f43a0681bd206df9fda865371d1ea92e35eb
Wang <wangliguo78@gmail.com>
2019-07-11 14:25:03 -0500

    CPAT v2.0.0

---
+++ setup.py
@@ -0,0 +1,48 @@
+import sys, os, platform, glob
+from distutils.core import setup
+from setuptools import *
+
+"""
+Setup script for CPAT  -- Coding Potential Assessment Tool
+"""
+
+def main():
+	setup(  name = "CPAT",
+			version = "2.0.0",
+			py_modules = [ 'psyco_full' ],
+			python_requires='>=3.5',
+			packages = find_packages( 'lib' ),
+			package_dir = { '': 'lib' },
+			package_data = { '': ['*.ps'] },
+			scripts = glob.glob( "bin/*.py"),
+			ext_modules = [],
+			test_suite = 'nose.collector',
+			setup_requires = ['nose>=0.10.4'],
+			author = "Liguo Wang, Jung Hyun Park",
+			author_email ="wangliguo78@gmail.com, hj_park@pitt.edu",
+			platforms = ['Linux','MacOS'],
+			requires = ['cython (>=0.17)'],
+			install_requires = ['numpy','pysam'], 
+			description = "CPAT (Coding Potential Assessment Tool)",
+			long_description = "CPAT is an alignment-free method to predict RNA coding potential using four sequence features.",
+			license='GNU General Public License',
+			url = "http://rna-cpat.sourceforge.net/",
+			zip_safe = False,
+			dependency_links = [],
+			classifiers=[
+				'Development Status :: 5 - Production/Stable',
+				'Environment :: Console',
+				'Intended Audience :: Science/Research',
+				'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+				'Operating System :: MacOS :: MacOS X',
+				'Operating System :: POSIX',
+				'Programming Language :: Python',
+				'Topic :: Scientific/Engineering :: Bio-Informatics',
+			],
+			
+			keywords='RNA coding potential prediction, lncRNA, lincRNA, logsitic regression',
+             )
+
+
+if __name__ == "__main__":
+	main()
---
+++ test/human_logit.make_logitModel.r
@@ -0,0 +1,4 @@
+data <- read.table(file="human_logit.feature.xls",sep="\t",header=T)
+attach(data)
+mylogit <- glm(Label ~ mRNA + ORF + Fickett + Hexamer, family=binomial(link="logit"), na.action=na.pass)
+save.image("human_logit.logit.RData")
