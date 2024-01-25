145f00f23dc6c6085b946827ac44634827dea44f
liguowang <liguowang@users.noreply.github.com>
2024-01-25 13:59:01 -0600

    version 3.0.5

--- test_files/human_train.make_logitModel.r
+++
@@ -1,4 +0,0 @@
-data <- read.table(file="human_train.feature.xls",sep="\t",header=T)
-attach(data)
-mylogit <- glm(Label ~ mRNA + ORF + Fickett + Hexamer, family=binomial(link="logit"), na.action=na.pass)
-save.image("human_train.logit.RData")
