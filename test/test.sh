f02c04edd8cd9bc90668779ada45631a4c354230
liguowang <liguowang@users.noreply.github.com>
2024-01-25 12:19:57 -0600

    v30.05

--- test/test.sh
+++
@@ -1,2 +0,0 @@
-python3 ../bin/cpat.py -x Human_Hexamer.tsv  -d  Human_logitModel.RData  --top-orf=5 -o out5   -g Human_test_coding_mRNA.fa --antisense
-python3 ../bin/cpat.py -x Human_Hexamer.tsv  -d  Human_logitModel.RData  --top-orf=10 -o out5   -g test4.fa  --antisens
---
+++ test_dat/human_train2.make_logitModel.r
@@ -0,0 +1,4 @@
+data <- read.table(file="human_train2.feature.xls",sep="\t",header=T)
+attach(data)
+mylogit <- glm(Label ~ mRNA + ORF + Fickett + Hexamer, family=binomial(link="logit"), na.action=na.pass)
+save.image("human_train2.logit.RData")
